2e715537e1b3d4377331df335d0124bebcded185
joxquinnn <joxquinnn1137@gmail.com>
2025-10-17 15:35:12 -0300

    fix: implementación dependencias/npm

---
+++ app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      backgroundColor: '#1f2937',
+      color: 'white',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '24px'
+    }}>
+    </div>
+  );
+}
+export default App;
